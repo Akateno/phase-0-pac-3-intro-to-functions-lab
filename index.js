937e2391c282a67f5208d0c8738dbf1475335d65
Erick Vargas <erickkvargas@gmail.com>
2022-03-13 18:45:37 -0700

    commit tree 235aacd430a3ff9f5f13c06f5980ca0631e5929e

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log("HELLO");
+}
+function logWhisper(string){
+    console.log("hello")}
+function sayHiToHeadphonedRoommate(string){
+switch (true) {
+    case string === "Let's have dinner together!":
+        return "I would love to!";
+        break;
+        case string === shout(string):
+            return "YES INDEED!";
+            break;
+            case string === whisper(string):
+            return "I can't hear you!";
+
+
+
+}
+}
